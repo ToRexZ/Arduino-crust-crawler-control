69b3bbab66ca4ee8e8672e8008dea2aab255d600
ToRexZ <victor.risager@hotmail.com>
2021-10-11 16:03:19 +0200

    Removed doublication of serial declaration. Now only declared in main.ino

--- src/Controller.hpp
+++ src/Controller.hpp
@@ -6,13 +6,6 @@
 
 #include "Joints.hpp"
 
-#if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_MEGA2560) // When using DynamixelShield
-#include <SoftwareSerial.h>
-#define DXL_SERIAL Serial
-#define DEBUG_SERIAL Serial3
-const uint8_t DIR_PIN = 2; // DYNAMIXEL Shield DIR PIN
-#endif
-
 class Controller
 {
 public:
@@ -20,8 +13,6 @@ public:
     ~Controller();
 
 private:
-    
-
 };
 
 #endif
